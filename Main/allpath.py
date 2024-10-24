f7daaabca95e4581074ae2fd0d9179af856e40c6
Mehmet Ali Koylu <koylu_m.ali@outlook.com>
2024-10-25 00:55:35 +0300

    Knife-Detector

--- Main/allpath.py
+++ Main/allpath.py
@@ -1,5 +1,6 @@
 import os
-model_path = os.path.join('.', 'Results', 'runs', 'detect', 'SGD_LR_0_005', 'weights', 'best.pt')
+model_path = os.path.join('.', 'Results', 'runs', 'detect', 'ADAMW_LR_0_001', 'weights', 'last.pt')
 output_image_path = 'Detected_knife/captured_knife.jpg'
-video_path = './video/video.mp4'
-output_path = './video/output_with_boxes.mp4'
+video_path = './video_and_photo/video.mp4'
+image_path = './video_and_photo/photo/2.jpg'
+output_path = './video_and_photo/output_with_boxes.mp4'
--- Training/main.py
+++ Training/main.py
@@ -11,11 +11,11 @@ print(f"Using device: {device}")
 model = YOLO("yolov8n.pt")
 
 # Eğer training_history.pkl dosyası varsa onu yükle, yoksa yeni bir dictionary başlat
-if os.path.exists('training_history.pkl'):
-    with open('training_history.pkl', 'rb') as f:
-        history = pickle.load(f)
-else:
-    history = {'train_loss': [], 'val_loss': [], 'metrics': []}
+# if os.path.exists('training_history.pkl'):
+#     with open('training_history.pkl', 'rb') as f:
+#         history = pickle.load(f)
+# else:
+#     history = {'train_loss': [], 'val_loss': [], 'metrics': []}
 
 # Eğitim fonksiyonu
 def train_model(optimizer_name, lr, epochs=100):
@@ -24,7 +24,7 @@ def train_model(optimizer_name, lr, epochs=100):
     # Seçilen optimizer ismi dinamik olarak kullanılıyor
     results = model.train(
         data="./training/data.yaml",
-        epochs=epochs,
+        epochs=1,
         optimizer=optimizer_name,  # 'Adam' yerine dinamik optimizer ismi
         imgsz=640,
         lr0=lr,
@@ -33,20 +33,58 @@ def train_model(optimizer_name, lr, epochs=100):
         workers=8
     )
     
+    print("-"*100)
+    #print(results.history.keys())
+    print("-"*100)
+    print(results.__dict__)
+    
+    train_loss_history = []
+    val_loss_history = []
+    mAP_history = []
+
+    # Eğitim sonuçlarını inceleyip kaydetme
+    for epoch in range(len(results)):
+        train_loss = results['train']['loss'][epoch]  # Her epoch için eğitim kaybı
+        val_loss = results['val']['loss'][epoch]  # Her epoch için validasyon kaybı
+        mAP_50_95 = results['metrics']['mAP_50_95'][epoch]  # Her epoch için mAP
+
+        # Geçmişi kaydet
+        train_loss_history.append(train_loss)
+        val_loss_history.append(val_loss)
+        mAP_history.append(mAP_50_95)
+
+
+    with open('first_training_history.pkl', 'wb') as f:
+        pickle.dump({
+            'train_loss': train_loss_history,
+            'val_loss': val_loss_history,
+            'mAP': mAP_history
+        }, f)
     # Eğitim sırasında sonuçlara results direkt olarak erişelim
-    try:
-        train_loss = results.loss['train']  # Eğitim kaybı
-        val_loss = results.loss['val']  # Doğrulama kaybı
-    except KeyError:
-        print("Train veya val loss bulunamadı. Sonuçları kontrol edin.")
-        train_loss, val_loss = None, None
+    # try:
+    #     # Box Loss (Bounding Box kaybı)
+    #     train_box_loss = results.box_loss
+    #     # Class Loss (Sınıflandırma kaybı)
+    #     train_cls_loss = results.cls_loss
+    #     # DFL Loss (Distribution Focal Loss kaybı)
+    #     train_dfl_loss = results.dfl_loss
+
+    #     val_box_loss = results.metrics['val/box_loss']
+    #     val_cls_loss = results.metrics['val/cls_loss']
+    #     val_dfl_loss = results.metrics['val/dfl_loss']
+
+    #     train_loss = results.history['train']  # Eğitim kaybı
+    #     val_loss = results.history['val']  # Doğrulama kaybı
+    # except KeyError:
+    #     print("Train veya val loss bulunamadı. Sonuçları kontrol edin.")
+    #     val_box_loss, val_cls_loss, val_dfl_loss = None, None, None
 
     # Eğitim süresince kaydedilen metrikleri dictionary'ye ekle
-    if train_loss is not None and val_loss is not None:
-        history['train_loss'].append(train_loss)
-        history['val_loss'].append(val_loss)
-        history['metrics'].append(results.metrics)  # Diğer metrikler
-    
+        # if train_loss is not None and val_loss is not None:
+        #     history['train_loss'].append(train_loss)
+        #     history['val_loss'].append(val_loss)
+        #     history['metrics'].append(results.metrics)  # Diğer metrikler
+        
     # Her model eğitimi sonrası modelin ağırlıklarını kaydet
     torch.save(model.model.state_dict(), f"./model_weights_{optimizer_name}_lr{lr}.pt")
     

b8f76b534ff817bf1ca6a1efef92c77c1d1e46e3
Mehmet Ali Koylu <91757385+MehmetAliKOYLU@users.noreply.github.com>
2024-10-25 02:29:02 +0300

    allpath.py

--- Main/allpath.py
+++ Main/allpath.py
@@ -1,6 +1,2 @@
 import os
 model_path = os.path.join('.', 'Results', 'runs', 'detect', 'ADAMW_LR_0_001', 'weights', 'last.pt')
-output_image_path = 'Detected_knife/captured_knife.jpg'
-video_path = './video_and_photo/video.mp4'
-image_path = './video_and_photo/photo/2.jpg'
-output_path = './video_and_photo/output_with_boxes.mp4'
